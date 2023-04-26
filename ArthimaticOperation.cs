7bb36bb9b77a2e213d3ddaf04711779a70f26dcd
ashut <ashut@LENOVO_IDEAPAD>
2023-04-27 02:36:47 +0530

    Add project files.

---
+++ ArthimaticOperation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ashutosh.Nayak.Day3.Exercise3
+{
+    public class ArthimaticOperation
+    {
+        
+        public double Add(double a,double b)
+        {
+           return a + b;
+        }
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+        public double Multiply(double a, double b) 
+        {
+            return a * b;
+        }
+        public double Divide(double a, double b) 
+        {
+            return a / b;
+        }
+        public double Modulus(double a, double b) 
+        {
+            return a % b;
+        }
+    }
+}
